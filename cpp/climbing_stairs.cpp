88b4f862ea05e72168cf6a464d6c1f899616a684
tayeva <124453543+tayeva@users.noreply.github.com>
2023-07-11 16:31:21 -0700

    initial

---
+++ cpp/climbing_stairs.cpp
@@ -0,0 +1,36 @@
+/*
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct
+ways can you climb to the top?
+
+Source: Leetcode
+*/
+
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+int distinct_ways_to_climb(int n) {
+  if (n <= 2)
+    return n;
+  std::vector<int> arr(n + 1);
+  arr[1] = 1;
+  arr[2] = 2;
+  for (int i = 3; i <= n; ++i) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n];
+}
+
+int main() {
+  int n;
+  std::cout << "Enter the number of steps: ";
+  std::cin >> n;
+  int ways = distinct_ways_to_climb(n);
+  std::cout << "Number of distinct ways to climb the staircase: " << ways
+            << std::endl;
+  assert(distinct_ways_to_climb(2) == 2);
+  assert(distinct_ways_to_climb(3) == 3);
+  return EXIT_SUCCESS;
+}
---
+++ cpp/contains_duplicate.cpp
@@ -0,0 +1,32 @@
+/*
+Given an integer array nums, return true if any value
+appears at least twice in the array, and return false
+if every element is distinct.
+*/
+
+#include <cassert>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+bool contains_duplicate(const std::vector<int> &nums) {
+  std::unordered_set<int> num_set;
+  for (const auto &num : nums) {
+    if (num_set.count(num) > 0) {
+      return true;
+    }
+    num_set.insert(num);
+  }
+  return false;
+}
+
+int main() {
+  std::vector<int> nums{1, 2, 3, 1};
+  bool result = contains_duplicate(nums);
+  std::cout << std::boolalpha << result << std::endl;
+  std::vector<int> dups{1, 2, 3, 1};
+  assert(contains_duplicate(dups) == true);
+  std::vector<int> no_dups{1, 2, 3, 4};
+  assert(contains_duplicate(no_dups) == false);
+  return EXIT_SUCCESS;
+}
