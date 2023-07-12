84f954a0847868cba0bc9c4297a2d14a9036e98c
tayeva <124453543+tayeva@users.noreply.github.com>
2023-07-12 09:22:01 -0700

    update source comment

--- cpp/climbing_stairs.cpp
+++ cpp/climbing_stairs.cpp
@@ -4,7 +4,7 @@ You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. In how many distinct
 ways can you climb to the top?
 
-Source: Leetcode
+Problem statement source: Leetcode
 */
 
 #include <cassert>
